fbba17be7d3fb86a438490ba60494ada92d4299a
avovana <vladimir.englishbox@gmail.com>
2018-07-10 11:23:51 +0400

    Improved Proxy Class.
    Divided data type to pointer and data. Added operator overloading functions.

--- include/proxy.cpp
+++ include/proxy.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <memory>
 
 namespace {
     template<typename T, unsigned N, typename... REST>
@@ -42,21 +43,79 @@ namespace {
     }
 }
 
-std::ostream &operator<<(std::ostream &output, const std::tuple<std::size_t, std::size_t> &instance) 
-{ 
-    output << std::get<0>(instance) << " " << std::get<1>(instance);
-    return output;
-}
+template<size_t CurrSize, typename Container>
+class ProxyData {
+    using gen_tuple = typename generate_tuple_type<size_t, CurrSize>::type;
+    using DataPointerType = std::shared_ptr<Container>;
 
-template<size_t CurrSize, size_t Size, typename Data, typename = void >
+    public:
+
+    template<typename... Args>
+    ProxyData(DataPointerType data_, Args... args) : data(data_), indexes(args...) {
+        //print(CurrSize, "ProxyData ctor", args...);
+    }
+
+    friend bool operator== (const ProxyData<CurrSize, Container>& instance, typename Container::mapped_type value)
+    {
+        typename Container::mapped_type currValue{};
+        
+        auto idx_iter = instance.data->find(instance.indexes);
+        
+        if(idx_iter != instance.data->end())
+            currValue = idx_iter->second;
+            
+        return currValue == value;
+    }
+
+    friend std::ostream &operator<<(std::ostream &output, const ProxyData<CurrSize, Container>& instance) {
+    
+        if(instance.data->find(instance.indexes) != instance.data->end())
+        {
+            //std::cout << "found" << '\n';
+            output << instance.data->operator [](instance.indexes);// << '\n';
+        }
+        else
+        {
+            //std::cout << "not found" << '\n';
+            typename Container::mapped_type mtype{};
+            output << mtype;//  << '\n';
+        }
+
+        return output;
+    }
+
+    auto operator = (typename Container::mapped_type value) {
+
+        typename Container::mapped_type default_value{};
+
+        if(value == default_value) {
+            auto idx_iter = data->find(indexes);
+            
+            if(idx_iter != data->end())
+                data->erase(idx_iter);
+                
+        } else {
+            (*data)[indexes] = value;
+        }
+
+        return *this;
+    }
+
+    private:
+    DataPointerType data;
+    gen_tuple indexes;
+};
+
+template<size_t CurrSize, size_t Size, typename Container, typename = void >
 class Proxy {
     using gen_tuple = typename generate_tuple_type<size_t, CurrSize>::type;
     using gen_tuple_next = typename generate_tuple_type<size_t, CurrSize + 1>::type;
+    using DataPointerType = std::shared_ptr<Container>;
     
     public:
     
     template<typename... Args>
-    Proxy(Data data_, Args... args) : data(data_), indexes(args...) {
+    Proxy(DataPointerType data_, Args... args) : data(data_), indexes(args...) {
         
         print(CurrSize, "basic template", args...);
     }
@@ -71,22 +130,23 @@ class Proxy {
     
     template<size_t... Is>
     auto prepare_for_constr(const gen_tuple_next& tuple, std::index_sequence<Is...>) const {
-        return Proxy<CurrSize + 1, Size, Data>(data, std::get<Is>(tuple)...);
+        return Proxy<CurrSize + 1, Size, Container>(data, std::get<Is>(tuple)...);
     }
 
-    Data data;
+    DataPointerType data;
     gen_tuple indexes;
 };
 
-template<size_t CurrSize, size_t Size, typename Data>
-class Proxy<CurrSize, Size, Data, typename std::enable_if<CurrSize == Size - 1>::type> {
+template<size_t CurrSize, size_t Size, typename Container>
+class Proxy<CurrSize, Size, Container, typename std::enable_if<CurrSize == Size - 1>::type> {
     using gen_tuple = typename generate_tuple_type<size_t, CurrSize>::type;
     using gen_tuple_next = typename generate_tuple_type<size_t, CurrSize + 1>::type;
+    using DataPointerType = std::shared_ptr<Container>;
     
     public:
     
     template<typename... Args>
-    Proxy(Data data_, Args... args) : data(data_), indexes(args...) {
+    Proxy(DataPointerType data_, Args... args) : data(data_), indexes(args...) {
         print(CurrSize, "specialized template", args...);
         
         //for(auto el : *data)
@@ -94,12 +154,18 @@ class Proxy<CurrSize, Size, Data, typename std::enable_if<CurrSize == Size - 1>:
     }
     
     auto operator[](const size_t index) const {
-        auto new_indexes = std::tuple_cat (indexes, std::tuple<size_t>(index));
+        auto new_indexes = std::tuple_cat(indexes, std::tuple<size_t>(index));
+        auto seq = std::make_index_sequence<CurrSize + 1>();
         
-        return (*data)[new_indexes];
+        return prepare_for_constr(new_indexes, seq);
     }
 
     private:
-    Data data;
+    DataPointerType data;
     gen_tuple indexes;
+    
+    template<size_t... Is>
+    auto prepare_for_constr(const gen_tuple_next& tuple, std::index_sequence<Is...>) const {
+        return ProxyData<CurrSize + 1, Container>(data, std::get<Is>(tuple)...);
+    }
 };
