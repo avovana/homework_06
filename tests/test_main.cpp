f301ecf4a20dcd30dcad5b6464baf748893bbf2e
Vladimir <vladimir.englishbox@gmail.com>
2018-07-13 13:24:46 +0400

    Fixed tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -13,23 +13,23 @@ BOOST_AUTO_TEST_SUITE(test_suite_main)
 
 BOOST_AUTO_TEST_CASE(index_generating)
 {
-    BOOST_CHECK(std::is_same< std::tuple<std::size_t>,                            Index>::value);
-    BOOST_CHECK(std::is_same< std::tuple<std::size_t, std::size_t>,               Indexes<2>>::value);
-    BOOST_CHECK(std::is_same< std::tuple<std::size_t, std::size_t, std::size_t>,  Indexes<3>>::value);
+    BOOST_CHECK(true == std::is_same< std::tuple<std::size_t>,                            Index>::value);
+    BOOST_CHECK(true == std::is_same< std::tuple<std::size_t, std::size_t>,               Indexes<2>>::value);
+    BOOST_CHECK(true == std::is_same< std::tuple<std::size_t, std::size_t, std::size_t>,  Indexes<3>>::value);
 
     using Concatenated2Indexes = decltype(addIndex<1>(Index{},      std::size_t{}));
     using Concatenated3Indexes = decltype(addIndex<2>(Indexes<2>{}, std::size_t{}));
     using Concatenated4Indexes = decltype(addIndex<3>(Indexes<3>{}, std::size_t{}));
 
-    BOOST_CHECK(std::is_same< Concatenated2Indexes,                               Indexes<2> >::value );
-    BOOST_CHECK(std::is_same< Concatenated3Indexes,                               Indexes<3> >::value );
-    BOOST_CHECK(std::is_same< Concatenated4Indexes,                               Indexes<4> >::value );
+    BOOST_CHECK(true == std::is_same< Concatenated2Indexes,                               Indexes<2> >::value );
+    BOOST_CHECK(true == std::is_same< Concatenated3Indexes,                               Indexes<3> >::value );
+    BOOST_CHECK(true == std::is_same< Concatenated4Indexes,                               Indexes<4> >::value );
 
     decltype(addIndex<1>(Index{}, std::size_t{})) indexes{5, 7};
-    BOOST_CHECK(compareIndexes<2>(indexes, "5, 7"));
+    BOOST_CHECK(true == compareIndexes<2>(indexes, "5, 7"));
 
     auto indexes2 = addIndex<2>(indexes, std::size_t{13});
-    BOOST_CHECK(compareIndexes<3>(indexes2, "5, 7, 13"));
+    BOOST_CHECK(true == compareIndexes<3>(indexes2, "5, 7, 13"));
 }
 
 BOOST_AUTO_TEST_CASE(matrix_assignement_operator)
